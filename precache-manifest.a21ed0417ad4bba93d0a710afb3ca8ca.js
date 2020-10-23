da38234b83fa37b9e0af6b94937343b5a5c5a46c
Franky Pan <frankypansiths@gmail.com>
2020-10-23 14:30:14 -0400

    Updates

---
+++ precache-manifest.a21ed0417ad4bba93d0a710afb3ca8ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad59287c9a24d92909ff7e3380c7653",
+    "url": "/c4me-FASS/index.html"
+  },
+  {
+    "revision": "d0895b376320d7e6fcf2",
+    "url": "/c4me-FASS/static/css/main.e30708b0.chunk.css"
+  },
+  {
+    "revision": "fb1f420007f72326dc6d",
+    "url": "/c4me-FASS/static/js/2.6f8ed5d6.chunk.js"
+  },
+  {
+    "revision": "97e477e72a3dc9a4e9004a620eb15c89",
+    "url": "/c4me-FASS/static/js/2.6f8ed5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0895b376320d7e6fcf2",
+    "url": "/c4me-FASS/static/js/main.f2429590.chunk.js"
+  },
+  {
+    "revision": "55ee5a130668d141305b",
+    "url": "/c4me-FASS/static/js/runtime-main.261210cf.js"
+  }
+]);
